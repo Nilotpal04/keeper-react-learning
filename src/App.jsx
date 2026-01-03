2a766a532d39756d65371e7a6002bd1b1dd47c94
Nilotpal04 <nilotpalb2004@gmail.com>
2026-01-03 23:11:09 +0530

    Added local-storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import Note from "./components/note";
 import Footer from "./components/footer";
 import CreateArea from "./components/createtextarea";
 import { useState } from "react";
+import {useEffect} from "react";
 
 function App() {
-  const [data, setData] = useState([]);
+
+  const [data, setData] = useState(() => {
+    const savedNotes = localStorage.getItem("notes");
+    return savedNotes ? JSON.parse(savedNotes) : [];
+  });
+
   function addItem(fullData) {
     setData((prev) => [...prev, fullData]);
   }
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(data));
+  }, [data]);
 
   function delBtn(id) {
     setData((prev) => {
@@ -18,7 +28,7 @@ function App() {
     });
   }
 
-  
+
   return (
     <div>
       <Header />
